544381a95b30d21e5f14f22aaf532af1e2e9f77e
TopuFD <topuroy3317@gmail.com>
2025-04-06 17:38:03 +0600

    updated

---
+++ input.py
@@ -0,0 +1,11 @@
+# num1 = int(input("Enter your number one :"))
+# num2 = int(input("Enter your number two :"))
+
+# sum = num1 + num2
+# print(sum)
+
+# desce = input("Enter your bio:")
+# print(desce)
+import random
+print(random.randrange(1,20))
+
